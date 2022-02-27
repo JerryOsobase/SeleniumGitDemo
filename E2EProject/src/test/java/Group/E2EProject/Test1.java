b5484d571d75c9f55fc5af167ee5f352a3ea915b
JerryOsobase <osobasejerry@gmail.com>
2022-02-27 22:48:18 +0100

    just for fun

---
+++ E2EProject/src/test/java/Group/E2EProject/Test1.java
@@ -0,0 +1,19 @@
+package Group.E2EProject;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void webAutomation() {
+		System.out.println("Chrome automation");
+		System.out.println("IE automation");
+		System.out.println("Safari automation");
+		
+	}
+	public void mobileAutomation() {
+		System.out.println("Android automation");
+		System.out.println("IOS automation");
+		
+	}
+}
